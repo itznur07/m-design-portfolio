3bf0ef483128685fb7954704bf11a8f5d4cce3be
Md Nuruddin <nuruddinalways99@gmail.com>
2023-01-17 00:40:34 +0600

    Skills components

---
+++ src/Components/Skills.js
@@ -0,0 +1,62 @@
+import React from "react";
+import Button from "./Button";
+
+// import icons
+import { FaCode, FaFigma, FaReact } from "react-icons/fa";
+
+const Skills = () => {
+  return (
+    <>
+      <div className='flex items-center justify-between gap-28 h-96'>
+        <div className='flex flex-col gap-6'>
+          <p className='text-md text-[#0db760] font-medium'>My Skills</p>
+          <p className='text-4xl font-bold leading-snug'>
+            Why Hire Me For Your <br />
+            Next <span className='text-[#0db760]'>Project?</span>
+          </p>
+          <p className='text-[#000] text-sm'>
+            I’m specialist in UI/UX Designe. My passion is designing  & solving{" "} problems through user <br /> experience and research.
+          </p>
+          <div className='cursor-pointer'>
+            <Button name='Contact me' />
+          </div>
+        </div>
+        <div className='flex items-center gap-8'>
+          <div className='flex flex-col items-center gap-16'>
+            <Card
+              icon={<FaReact />}
+              title='React Native'
+              about='Create user interface design with unique & modern ideas'
+            />
+            <Card
+              icon={<FaFigma />}
+              title='Figma'
+              about='Create user interface design with unique & modern ideas'
+            />
+          </div>
+          <div>
+            <Card
+              icon={<FaCode />}
+              title='MERN Stack'
+              about='Create user interface design with unique & modern ideas'
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+const Card = ({ icon, title, about }) => {
+  return (
+    <>
+      <div className='flex flex-col gap-2 shadow-md px-5 py-4 rounded-sm'>
+        <i className='text-xl text-[#0db760]'>{icon}</i>
+        <p className='text-md font-medium'>{title}</p>
+        <p className='text-sm'>{about}</p>
+      </div>
+    </>
+  );
+};
+
+export default Skills;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Banner from "../Components/Banner";
 import Navbar from "../Components/Navbar";
 import Rewards from "../Components/Rewards";
+import Skills from "../Components/Skills";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Navbar />
       <Banner />
       <Rewards />
+      <Skills />
     </>
   );
 };
